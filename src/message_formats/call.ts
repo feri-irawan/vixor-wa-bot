bb1a6a6111033a3a3c9b7c2be1021e02336676e6
Feri Irawan <feriirawan.akun@gmail.com>
2023-01-07 02:27:45 +0000

    reject call

--- src/message_formats/call.ts
+++ src/message_formats/call.ts
@@ -1,7 +1,9 @@
 import { WASocket } from "@adiwajshing/baileys";
 
-export default function call(sock: WASocket, data: any) {
-  const { chatId, from, status } = data[0];
+export default async function call(sock: WASocket, data: any) {
+  const { chatId, from, status, id } = data[0];
+
+  await sock.rejectCall(id, from);
 
   if ((status === "timeout" || status === "reject") && from === chatId)
     sock.sendMessage(from, {
